6cdb8fc00703e5f13f172b2e8195595704ab3439
koallider <xehxtkf@gmail.com>
2022-11-07 20:40:06 +0300

    Flutter Yandex Games Plugin Example

---
+++ example/README.md
@@ -0,0 +1,7 @@
+# flutter_yandex_games example
+
+Demonstrates how to use the flutter_yandex_games plugin.
+
+Use this link to run the example from local server.
+
+https://yandex.ru/dev/games/doc/dg/concepts/local-launch.html
---
+++ example/lib/main.dart
@@ -0,0 +1,107 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_yandex_games/yandex_games.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  String status = "Initializing...";
+
+  bool initFinished = false;
+
+  @override
+  void initState() {
+    initYandexGames();
+  }
+
+  void initYandexGames(){
+    YandexGames.init().then((value){
+      setState(() {
+        status = "Yandex Games Sdk Init Successful";
+        initFinished = true;
+      });
+    });
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Text(status),
+          TextButton(onPressed: initFinished ? showFullscreenAd : null, child: const Text("Show Fullscreen Ad")),
+          TextButton(onPressed: initFinished ? showRewardedVideoAd : null, child: const Text("Show Rewarded Video Ad")),
+
+          TextButton(onPressed: initFinished ? savePlayerData : null, child: const Text("Save Status To Player Data")),
+          TextButton(onPressed: initFinished ? getPlayerData : null, child: const Text("Load Player Data")),
+        ],
+      ),
+    );
+  }
+
+  void showFullscreenAd(){
+    YandexGames.showFullscreenAd();
+  }
+
+  void showRewardedVideoAd() {
+    YandexGames.showRewardedVideoAd(
+      onOpen: (){
+        setState(() {
+          status = "Rewarded Video Open";
+        });
+      },
+      onRewarded: (){
+        setState(() {
+          status = "Rewarded Video Rewarded";
+        });
+      },
+      onClose: (){
+
+      },
+      onError: (){
+        setState(() {
+          status = "Rewarded Video Not Loaded";
+        });
+      },
+    );
+  }
+
+  void savePlayerData(){
+    YandexGames.getPlayer().setData({"status": status});
+  }
+
+  void getPlayerData(){
+    YandexGames.getPlayer().getData().then((value) {
+      setState(() {
+        status = "Player Data Received: ${jsonEncode(value)}";
+      });
+    });
+  }
+}
