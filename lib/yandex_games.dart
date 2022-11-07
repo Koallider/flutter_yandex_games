834453e5743c4a401c85ca256ba84d526b304d00
koallider <xehxtkf@gmail.com>
2022-11-07 20:15:41 +0300

    cleanup

--- lib/yandex_games.dart
+++ lib/yandex_games.dart
@@ -7,38 +7,37 @@ import 'dart:js_util';
 
 import 'package:js/js.dart';
 
-void initializeYandexGamesPlugin() {}
-
-Map mapify(JsObject obj) {
-  return jsonDecode(context['JSON'].callMethod('stringify', [obj]));
-}
-
 class YandexGames {
+
+  /// Initializes the Yandex Games sdk and gets the player object.
+  /// Call this on start of your application.
   static Future<bool> init() async {
-    _yaSdk = await promiseToFuture(YaGamesJs.init());
+    _yaSdk = await promiseToFuture(_YaGamesJs.init());
     _player = Player(await promiseToFuture(
-        _yaSdk.getPlayer(GetPlayerOptions(scopes: false))));
+        _yaSdk.getPlayer(_GetPlayerOptions(scopes: false))));
     return true;
   }
 
-  static late YaSdk _yaSdk;
+  static late _YaSdk _yaSdk;
   static late Player _player;
 
   static Player getPlayer() {
     return _player;
   }
 
+  ///Displays Fullscreen AD
   static void showFullscreenAd() {
     _yaSdk.adv.showFullscreenAdv();
   }
 
+  /// Displays Rewarded Video AD and calls callback functions on Ad events
   static void showRewardedVideoAd(
       {required Function onOpen,
       required Function onRewarded,
       required Function onClose,
       required Function onError}) {
-    _yaSdk.adv.showRewardedVideo(ShowRewardedVideoOptions(
-      callbacks: RewardedVideoCallbacks(
+    _yaSdk.adv.showRewardedVideo(_ShowRewardedVideoOptions(
+      callbacks: _RewardedVideoCallbacks(
         onOpen: allowInterop<Function>(onOpen),
         onRewarded: allowInterop<Function>(onRewarded),
         onClose: allowInterop<Function>(onClose),
@@ -49,20 +48,30 @@ class YandexGames {
 }
 
 class Player {
-  YaPlayer player;
+  _YaPlayer player;
 
   Player(this.player);
 
+  //Returns player's ID
   String getUniqueID() => player.getUniqueID();
 
+  /// Sets player data.
+  ///
+  /// Be sure to include all data when you use it.
+  /// Data will not be merged with existing data.
+  /// If [flush] is true sends the data immediately.
   void setData(Map<dynamic, dynamic> data, {bool flush = false}) {
     var newData = jsify(data);
     player.setData(newData, flush);
   }
 
+  /// Sets player data.
+  ///
+  /// Be sure to include all data when you use it.
+  /// Data will not be merged with existing data.
   Future<Map<dynamic, dynamic>> getData() async {
     JsObject data = await promiseToFuture<JsObject>(player.getData());
-    var map = mapify(data);
+    var map = _mapify(data);
     //Dart objects contain a JS object in 'o' field? Have to do this for now.
     if (map["o"] != null) {
       return map["o"];
@@ -71,13 +80,18 @@ class Player {
   }
 }
 
+///Converts JS object to Map
+Map _mapify(JsObject obj) {
+  return jsonDecode(context['JSON'].callMethod('stringify', [obj]));
+}
+
 @JS("YaGames")
-class YaGamesJs {
+class _YaGamesJs {
   external static Object init();
 }
 
 @JS("Player")
-class YaPlayer {
+class _YaPlayer {
   external String getUniqueID();
 
   external void setData(JsObject data, bool flush);
@@ -86,37 +100,37 @@ class YaPlayer {
 }
 
 @JS("YaSdk")
-class YaSdk {
-  external Object getPlayer(GetPlayerOptions options);
+class _YaSdk {
+  external Object getPlayer(_GetPlayerOptions options);
 
-  external YaAdv get adv;
+  external _YaAdv get adv;
 }
 
 @JS("Adv")
-class YaAdv {
+class _YaAdv {
   external void showFullscreenAdv();
 
-  external void showRewardedVideo(ShowRewardedVideoOptions options);
+  external void showRewardedVideo(_ShowRewardedVideoOptions options);
 }
 
 @anonymous
 @JS()
-class GetPlayerOptions {
+class _GetPlayerOptions {
   external bool get scopes;
 
-  external factory GetPlayerOptions({bool scopes});
+  external factory _GetPlayerOptions({bool scopes});
 }
 
 @anonymous
 @JS()
-class ShowRewardedVideoOptions {
-  external factory ShowRewardedVideoOptions({RewardedVideoCallbacks callbacks});
+class _ShowRewardedVideoOptions {
+  external factory _ShowRewardedVideoOptions({_RewardedVideoCallbacks callbacks});
 }
 
 @anonymous
 @JS()
-class RewardedVideoCallbacks {
-  external factory RewardedVideoCallbacks(
+class _RewardedVideoCallbacks {
+  external factory _RewardedVideoCallbacks(
       {Function onOpen,
         Function onRewarded,
         Function onClose,
